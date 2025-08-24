dfa09492bdb49f708a2e76d1b76d53eac4d60477
takagidenki-densensentei <takagidenkisk@gmail.com>
2025-08-25 08:58:37 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('cable-app-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
